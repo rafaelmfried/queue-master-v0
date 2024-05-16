eb6b0c564f666aa9f350e2d34d2c8008138e32b9
Rafael Moura Friederick <rafael.mf.documents@gmail.com>
2024-05-16 20:02:42 -0300

    feat: create client class, this class need to provide the client features

---
+++ lib/classes/client.rb
@@ -0,0 +1,23 @@
+require 'securerandom'
+require 'classes/ticket'
+
+class Client
+  class << self
+    def create_client(queue)
+      client = new
+      client.request_ticket(queue)
+      client
+    end
+  end
+
+  attr_reader :id, :ticket
+
+  def initialize
+    @id = SecureRandom.uuid
+    @ticket = nil
+  end
+
+  def request_ticket(queue)
+    @ticket = queue.add_ticket
+  end
+end
